bcf9c05ca97205d14ce5c4457d9c6f37db7df28e
aribrilliantsyah <151610183@smkassalaambandung.sch.id>
2017-07-19 10:59:41 +0700

    hari kedua

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     <div class="container">
         <div class="row">
           <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-custom">
+                <div class="panel panel-primary">
                   <div class="panel-heading panel-heading-custom">Halo user </div>
                   <div class="panel-body">
                       Selamat Datang Di Rahmat Padil Motor
